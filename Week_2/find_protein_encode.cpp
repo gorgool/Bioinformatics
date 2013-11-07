ecc909b5f399c24b2a41e921938740d646923ba3
Gorgool <gorgoolstud@gmail.com>
2013-11-07 22:51:24 +0400

    Week 2. 2 Problem complete

--- Week_2/find_protein_encode.cpp
+++ Week_2/find_protein_encode.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -14,26 +15,60 @@ static string dna_to_rna(const string& dna)
   return ret;
 }
 
-string to_protein(const string& rna);
+map<char, char> pairs =
+{
+  make_pair('A', 'T'),
+  make_pair('T', 'A'),
+  make_pair('C', 'G'),
+  make_pair('G', 'C')
+};
+
+static string reverse_complement(const string& input_str)
+{
+  string ret(input_str);
+  for (size_t i = 0; i < ret.length(); i++)
+  {
+    ret[i] = pairs[ret[i]];
+  }
+  return string(ret.rbegin(), ret.rend());
+}
+
+extern map<string, char> protein_tbl;
+static string to_protein(const string& input_str)
+{
+  string ret;
+  size_t pos = 0;
+  string codone(input_str.substr(pos, 3));
+
+  while (pos + 3 <= input_str.length())
+  {
+    ret += protein_tbl[codone];
+
+    pos += 3;
+    codone = input_str.substr(pos, 3);
+  }
+
+  return ret;
+}
 
 vector<string> find_protein_encode(const string& genome, const string& proteins)
 {
-  string rna(dna_to_rna(genome));
-
-  string protein_str = to_protein(rna);
-
   vector<string> ret;
 
-  int pos = -1;
-
-  while (true)
+  string substr, peptide, reverse_peptide;
+  for (size_t i = 0; i < genome.length(); i++)
   {
-    pos = protein_str.find(proteins, pos + 1);
+    substr = genome.substr(i, proteins.length() * 3);
 
-    if (pos == string::npos)
-      break;
+    peptide = to_protein(dna_to_rna(substr));
+    reverse_peptide = to_protein(dna_to_rna(reverse_complement(substr)));
 
-    ret.push_back(string(genome.begin() + pos, genome.begin() + pos + proteins.length() * 3));\
+    if (
+      strcmp(peptide.c_str(), proteins.c_str()) == 0 ||
+      strcmp(reverse_peptide.c_str(), proteins.c_str()) == 0)
+    {
+      ret.push_back(substr);
+    }
   }
 
   return ret;
