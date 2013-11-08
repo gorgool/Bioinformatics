f78c302f01fe478a322fd82cccc66d172bd51f1b
Gorgool <gorgoolstud@gmail.com>
2013-11-08 22:09:37 +0400

    Week 2. Problem 4

--- Week_2/generate_cyclopeptide.cpp
+++ Week_2/generate_cyclopeptide.cpp
@@ -1,19 +1,51 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <map>
+#include <set>
 
 using namespace std;
 
-vector<vector<size_t>> generate_cyclopeptide(vector<size_t> mass_spectrum)
+static vector<set<size_t>> next_seq(set<size_t> start_seq, size_t specturm_length, size_t step)
 {
-  vector<size_t> spec_perm(mass_spectrum);
+  vector<set<size_t>> ret;
 
-  vector<vector<size_t>> ret;
-
-  do 
+  for (size_t i = 0; i < specturm_length; i++)
   {
-    ret.push_back(spec_perm);
-  } while (next_permutation(spec_perm.begin(), spec_perm.end()));
-  
+      set<size_t> new_seq(start_seq);
+      new_seq.insert(i);
+
+      if (new_seq.size() == step)
+        ret.push_back(new_seq);
+  }
+
   return ret;
+}
+
+vector<vector<size_t>> generate_cyclopeptide(vector<size_t>& mass_spectrum)
+{
+  vector<size_t> spec;
+  for (auto mass : mass_spectrum)
+  {
+    if (mass > 186)
+      break;
+
+    if (mass !=0)
+      spec.push_back(mass);
+  }
+  
+  map<size_t, size_t> mass_tbl;
+
+  for (size_t i = 0; i < spec.size(); i++)
+    mass_tbl.insert({ i, spec[i] });
+
+  vector<set<size_t>> list{ {0} };
+
+  list = next_seq(list[0], spec.size(), 2);
+
+  list = next_seq(list[5], spec.size(), 3);
+
+  list = next_seq(list[2], spec.size(), 4);
+  
+  return vector<vector<size_t>>();
 }
