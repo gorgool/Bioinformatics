eefa6656b1b21676fb18636c4f380e7fa6806f26
gorgool <gorgoolstud@gmail.com>
2014-01-13 16:55:51 +0400

    Week 9. Problem 2 Complete
    Bruteforce

--- Week_9/main.cpp
+++ Week_9/main.cpp
@@ -5,12 +5,13 @@
 
 using namespace std;
 
-list<vector<size_t>> trie_matching(const string& text, const vector<string> &patterns);
+vector<size_t> trie_matching(const string& text, const vector<string> &patterns);
 
 int main()
 {
   string text;
   cin >> text;
+  cin.ignore();
   vector<string> patterns;
   string buff;
   while(getline(cin, buff))
@@ -19,10 +20,10 @@ int main()
   auto ret = trie_matching(text, patterns);
   for (const auto& item : ret)
   {
-    for (const auto idx : item)
-      cout << idx << " ";
-    cout << endl;
-  } 
+    cout << item << " ";
+  }
+
+  // 108 115 312 319 375 382 500 565 1178 1185 1265 1858 2113 3107 3229 3236 3525 3537 3668 3675 3722 3729 4144 4151 4291 4315 4477 5345 5352 5664 6449 6456 6652 6659 6721 7105 7424 7431 7800 7868 7875 8078 8304 8311 8523 8536
   
   return 0;
 }
--- Week_9/trie_matching.cpp
+++ Week_9/trie_matching.cpp
@@ -4,14 +4,17 @@
 
 using namespace std;
 
-trie trie_construct(const vector<string>& vs);
-
-list<vector<size_t>> trie_matching(const string& text, const vector<string> &patterns)
+vector<size_t> trie_matching(const string& text, const vector<string> &patterns)
 {
-  trie t = trie_construct(patterns);
-
+  vector<size_t> ret;
   for (size_t i = 0; i < text.length(); i++)
   {
-    
+    for (const auto& pattern : patterns)
+    {
+      auto substr = text.substr(i, pattern.length());
+      if (pattern == substr)
+        ret.push_back(i);
+    }
   }
+  return ret;
 }
